880fd1c0c6e1a78aaa0af09e8b0f056b8e281ed4
Joe <chanaphat.sip@gmail.com>
2024-08-27 02:53:26 +0700

    bp-5-joe

--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,15 @@
 // Exercise #3: Find a Minimum Score
-let scores = [100, 20, 3, 1000];
+let scores = [100, 20, 3, 1000,];
 let minScore;
 // Start coding here
-
+let i = 0
+let j = 0
+while (j < scores.length){
+    if (scores[i] <= scores[j]){
+        j++;
+    } else {
+        i++;
+    }
+};
+minScore = scores[i];
 console.log(minScore);
--- ex-6.js
+++ ex-6.js
@@ -3,5 +3,7 @@ let companyName = "TechUp Thailand";
 let reversedCompanyName = "";
 
 // Start coding here
-
-console.log(reversedCompanyName);
+for (let i = companyName.length-1 ; i >=0  ; i--){
+    reversedCompanyName = reversedCompanyName + companyName[i];
+};
+console.log(reversedCompanyName)
